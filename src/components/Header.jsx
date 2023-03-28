b8f687867f034b847e65573a92eb274e9f466f9e
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-03-28 20:34:59 +0300

    adding CRA

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <div>
+      <Header>
+        <Logo>
+          <span role="img" aria-label="computer icon">
+            💻
+          </span>{' '}
+          GoMerch Store
+        </Logo>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+      </Header>
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
+}
