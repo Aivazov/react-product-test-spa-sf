a94ca8a0619d92ebc27f62a42e0dda82bad156ce
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-03-29 16:01:18 +0300

    adding table

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import NoImage from '../assets/images/no-image.png';
 
 export default function Products() {
   return (
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-md-6 text-center mb-5">
-          <h2 className="heading-section">Table #05</h2>
+          <h2 className="text-start">Products</h2>
         </div>
       </div>
       <div className="row">
@@ -14,32 +15,47 @@ export default function Products() {
             <table className="table table-responsive-xl">
               <thead>
                 <tr>
-                  <th>&nbsp;</th>
-                  <th>Email</th>
-                  <th>Username</th>
-                  <th>Status</th>
-                  <th>&nbsp;</th>
+                  <th className="pr-2">ID</th>
+                  <th className="pr-2">Photo</th>
+                  <th className="pr-2">Name</th>
+                  <th className="pr-2 max-w-[200px]">Description</th>
+                  <th className="pr-2">Price</th>
+                  <th className="pr-2">Rating</th>
+                  <th className="pr-2">Stock</th>
+                  <th>Category</th>
                 </tr>
               </thead>
               <tbody>
                 <tr className="alert" role="alert">
                   <td>
-                    <label className="checkbox-wrap checkbox-primary">
-                      <input type="checkbox" checked />
-                      <span className="checkmark"></span>
+                    <label className="product-id">
+                      <span className="checkmark">1</span>
                     </label>
                   </td>
-                  <td className="d-flex align-items-center">
+                  <td className="flex items-center justify-start">
                     <div className="img">
-                      <img src="/public/images/no-image.png" alt="" />
-                    </div>
-                    <div className="pl-3 email">
-                      <span>markotto@email.com</span>
+                      <img src={NoImage} width={50} height={50} alt="" />
                     </div>
                   </td>
-                  <td>Markotto89</td>
-                  <td className="status">
-                    <span className="active">Active</span>
+                  <td className="name">
+                    <span>Apple Iphone 13</span>
+                  </td>
+                  <td className="description max-w-[200px]">
+                    <span className="active">
+                      An apple mobile which is nothing like apple
+                    </span>
+                  </td>
+                  <td className="price">
+                    <span className="active">$549</span>
+                  </td>
+                  <td className="rating">
+                    <span className="active">4.69</span>
+                  </td>
+                  <td className="stock">
+                    <span className="active">94</span>
+                  </td>
+                  <td className="category">
+                    <span className="active">smartphones</span>
                   </td>
                   <td>
                     <button
@@ -54,122 +70,39 @@ export default function Products() {
                     </button>
                   </td>
                 </tr>
+
                 <tr className="alert" role="alert">
                   <td>
-                    <label className="checkbox-wrap checkbox-primary">
-                      <input type="checkbox" />
-                      <span className="checkmark"></span>
+                    <label className="product-id">
+                      <span className="checkmark">1</span>
                     </label>
                   </td>
-                  <td className="d-flex align-items-center">
+                  <td className="flex items-center justify-start">
                     <div className="img">
-                      <img src="/public/images/no-image.png" alt="" />
-                    </div>
-                    <div className="pl-3 email">
-                      <span>jacobthornton@email.com</span>
+                      <img src={NoImage} width={50} height={50} alt="" />
                     </div>
                   </td>
-                  <td>Jacobthornton</td>
-                  <td className="status">
-                    <span className="waiting">Waiting for Resassignment</span>
+                  <td className="name">
+                    <span>Apple Iphone 13</span>
+                  </td>
+                  <td className="description max-w-[200px]">
+                    <span className="active">
+                      An apple mobile which is nothing like apple
+                    </span>
+                  </td>
+                  <td className="price">
+                    <span className="active">$549</span>
+                  </td>
+                  <td className="rating">
+                    <span className="active">4.69</span>
+                  </td>
+                  <td className="stock">
+                    <span className="active">94</span>
+                  </td>
+                  <td className="category">
+                    <span className="active">smartphones</span>
                   </td>
                   <td>
-                    <button
-                      type="button"
-                      className="close"
-                      data-dismiss="alert"
-                      aria-label="Close"
-                    >
-                      <span aria-hidden="true">
-                        <i className="fa fa-close"></i>
-                      </span>
-                    </button>
-                  </td>
-                </tr>
-                <tr className="alert" role="alert">
-                  <td>
-                    <label className="checkbox-wrap checkbox-primary">
-                      <input type="checkbox" />
-                      <span className="checkmark"></span>
-                    </label>
-                  </td>
-                  <td className="d-flex align-items-center">
-                    <div className="img">
-                      <img src="/public/images/no-image.png" alt="" />
-                    </div>
-                    <div className="pl-3 email">
-                      <span>larrybird@email.com</span>
-                    </div>
-                  </td>
-                  <td>Larry_bird</td>
-                  <td className="status">
-                    <span className="active">Active</span>
-                  </td>
-                  <td>
-                    <button
-                      type="button"
-                      className="close"
-                      data-dismiss="alert"
-                      aria-label="Close"
-                    >
-                      <span aria-hidden="true">
-                        <i className="fa fa-close"></i>
-                      </span>
-                    </button>
-                  </td>
-                </tr>
-                <tr className="alert" role="alert">
-                  <td>
-                    <label className="checkbox-wrap checkbox-primary">
-                      <input type="checkbox" />
-                      <span className="checkmark"></span>
-                    </label>
-                  </td>
-                  <td className="d-flex align-items-center">
-                    <div className="img">
-                      <img src="/public/images/no-image.png" alt="" />
-                    </div>
-                    <div className="pl-3 email">
-                      <span>johndoe@email.com</span>
-                    </div>
-                  </td>
-                  <td>Johndoe1990</td>
-                  <td className="status">
-                    <span className="active">Active</span>
-                  </td>
-                  <td>
-                    <button
-                      type="button"
-                      className="close"
-                      data-dismiss="alert"
-                      aria-label="Close"
-                    >
-                      <span aria-hidden="true">
-                        <i className="fa fa-close"></i>
-                      </span>
-                    </button>
-                  </td>
-                </tr>
-                <tr className="alert" role="alert">
-                  <td className="border-bottom-0">
-                    <label className="checkbox-wrap checkbox-primary">
-                      <input type="checkbox" />
-                      <span className="checkmark"></span>
-                    </label>
-                  </td>
-                  <td className="d-flex align-items-center border-bottom-0">
-                    <div className="img">
-                      <img src="/public/images/no-image.png" alt="" />
-                    </div>
-                    <div className="pl-3 email">
-                      <span>garybird@email.com</span>
-                    </div>
-                  </td>
-                  <td className="border-bottom-0">Garybird_2020</td>
-                  <td className="status border-bottom-0">
-                    <span className="waiting">Waiting for Resassignment</span>
-                  </td>
-                  <td className="border-bottom-0">
                     <button
                       type="button"
                       className="close"
