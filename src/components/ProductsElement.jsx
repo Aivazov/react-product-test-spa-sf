6f7401d80ed3d4e4695cb481642471651b668e94
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-03-29 22:46:32 +0300

    structuring adding form

--- src/components/ProductsElement.jsx
+++ src/components/ProductsElement.jsx
@@ -5,7 +5,7 @@ import { GrClose } from 'react-icons/gr';
 export default function ProductsElement({ data }) {
   return (
     // <tr className="grid grid-cols-9" role="alert">
-      <tr className="flex items-center justify-between" role="alert">
+    <tr className="flex items-center justify-between bg-gray-400 mb-2" role="alert">
       <td>
         <label className="product-id">
           <span className="checkmark">{data.id}</span>
@@ -13,8 +13,8 @@ export default function ProductsElement({ data }) {
       </td>
       <td className="flex items-center justify-start">
         <div className="img">
-          {data.images[0] ? (
-            <img src={data.images[0]} width={50} height={50} alt="" />
+          {data.thumbnail ? (
+            <img src={data.thumbnail} width={50} height={50} alt="" />
           ) : (
             <img src={NoImage} width={50} height={50} alt="" />
           )}
@@ -24,7 +24,7 @@ export default function ProductsElement({ data }) {
         <span>{data.title}</span>
       </td>
       <td className="description max-w-[200px]">
-      {/* <td className="description max-w-[200px] overflow-y-scroll overflow-x-hidden"> */}
+        {/* <td className="description max-w-[200px] overflow-y-scroll overflow-x-hidden"> */}
         <span className="active">{data.description}</span>
       </td>
       <td className="price">
--- src/pages/AddProductForm.jsx
+++ src/pages/AddProductForm.jsx
@@ -2,28 +2,86 @@ import React from 'react';
 
 export default function AddProductForm() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
       <h1>Adding of the product</h1>
-      <form action="">
-        <label htmlFor="">
-          <input type="text" placeholder="Name" />
-        </label>
-        <label htmlFor="">
-          <input type="num" placeholder="Price" />
-        </label>
-        <label htmlFor="">
-          <input type="num" placeholder="Rating" />
-        </label>
-        <label htmlFor="">
-          <input type="num" placeholder="Stock" />
-        </label>
-        <label htmlFor="">
-          <input type="text" placeholder="Category" />
-        </label>
-        <label htmlFor="">
-          <textarea type="text" placeholder="Description" />
-        </label>
-      </form>
+      <div className="flex justify-center items-center">
+        <form
+          action=""
+          className="flex flex-col justify-start items-stretch space-y-3"
+        >
+          <div>
+            <label>
+              <input
+                type="text"
+                placeholder="Name"
+                className="border border-black rounded-lg py-2 px-4 text-start mr-4"
+              />
+            </label>
+            <label>
+              <input
+                type="text"
+                placeholder="Author"
+                className="border border-black rounded-lg py-2 px-4 text-start"
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              <input
+                type="date"
+                placeholder="Date"
+                className="border border-black rounded-lg py-2 px-4 text-start mr-4"
+              />
+            </label>
+            <label>
+              <input
+                type="number"
+                placeholder="Price"
+                className="border border-black rounded-lg py-2 px-4 text-start"
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              <input
+                type="number"
+                placeholder="Rating"
+                className="border border-black rounded-lg py-2 px-4 text-start mr-4"
+              />
+            </label>
+            <label>
+              <input
+                type="number"
+                placeholder="Stock"
+                className="border border-black rounded-lg py-2 px-4 text-start"
+              />
+            </label>
+          </div>
+          <label>
+            <input
+              type="text"
+              placeholder="Category"
+              className="border border-black rounded-lg py-2 px-4 text-start w-full"
+            />
+          </label>
+          <label className="">
+            <textarea
+              type="text"
+              placeholder="Description"
+              className="border border-black rounded-lg py-2 px-4 text-start w-full"
+            />
+          </label>
+          <button
+            type="submit"
+            className="w-full bg-black rounded-lg text-white py-3 hover:bg-gray-600 transition-all ease duration-200"
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+          >
+            Add Product
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
